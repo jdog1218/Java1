7ae32a3e2a866d4ecaef61e8336fa682e72b80a5
Joel <Joel@10.0.84.219>
2017-03-09 13:33:13 -0800

    Nested Shapes finished

--- IC10_NestedShapes/src/edu/orangecoastcollege/cs170/jgordon41/ic10/NestedShapes.java
+++ IC10_NestedShapes/src/edu/orangecoastcollege/cs170/jgordon41/ic10/NestedShapes.java
@@ -30,9 +30,16 @@ public class NestedShapes {
 			System.out.println();
 		}
 
+		for(int row = 1; row <= 4; row++){
+			for(int col = 1; col <= row; col++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
 		//for the prymid shape
 		for(int row = 1; row <= 4; row++){
-			for(int col = 1; col <=4; col++){
+			for(int col = 1; col <= row; col++){
 				System.out.print(row);
 			}
 			System.out.println();
