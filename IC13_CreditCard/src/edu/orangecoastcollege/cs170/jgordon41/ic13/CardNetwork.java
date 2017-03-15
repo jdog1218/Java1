65189ae871d6bae7e547ef2d65bc01adb57d99af
Joel <Joel@10.0.84.20>
2017-03-14 18:11:24 -0700

    Sharing

---
+++ IC13_CreditCard/src/edu/orangecoastcollege/cs170/jgordon41/ic13/CardNetwork.java
@@ -0,0 +1,20 @@
+/**
+ * Gordon, Joel
+ * CS170
+ * Mar-14-2017
+ * 
+ * IC13_CreditCard
+ */
+package edu.orangecoastcollege.cs170.jgordon41.ic13;
+
+/**
+ * @author Joel
+ *
+ */
+public enum CardNetwork {
+
+	AMEX,
+	DISCOVER,
+	MASTER_CARD,
+	VISA
+}
---
+++ IC13_CreditCard/src/edu/orangecoastcollege/cs170/jgordon41/ic13/CreditCardDemo.java
@@ -0,0 +1,25 @@
+/**
+ * Gordon, Joel
+ * CS170
+ * Mar-14-2017
+ * 
+ * IC13_CreditCard
+ */
+package edu.orangecoastcollege.cs170.jgordon41.ic13;
+
+/**
+ * @author Joel
+ *
+ */
+public class CreditCardDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CardNetwork network = CardNetwork.AMEX;
+		CreditCard card1 = new CreditCard(network, "Joel Gordon", "08/19", "8765435476545654", 768 );
+
+	}
+
+}
