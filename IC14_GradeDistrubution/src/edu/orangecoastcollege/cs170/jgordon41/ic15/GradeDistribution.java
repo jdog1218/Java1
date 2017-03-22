630a7d67c0a9a446e82c959f15f597e997b33848
Joel <Joel@JoelJR-PC>
2017-03-22 12:04:17 -0700

    Grade Distribution, A little closer

--- IC14_GradeDistrubution/src/edu/orangecoastcollege/cs170/jgordon41/ic15/GradeDistribution.java
+++ IC14_GradeDistrubution/src/edu/orangecoastcollege/cs170/jgordon41/ic15/GradeDistribution.java
@@ -81,10 +81,15 @@ public class GradeDistribution
         this.mNumberFs = mNumberFs;
     }
 
-    public void setAllGrades(){
-        
+    public void setAllGrades(int number){
+    	this.mNumberAs = number;
+        this.mNumberBs = number;
+        this.mNumberCs = number;
+        this.mNumberDs = number;
+        this.mNumberFs = number;
     }
 
+    
     public boolean equals(GradeDistribution obj)
     {
         if (mNumberAs == obj.mNumberAs) return true;
@@ -95,6 +100,39 @@ public class GradeDistribution
         return false;
     }
     
+    public String toString(){
+    	String result = "";
+    	
+    	for(int i = 1; i <= mNumberAs; i++)
+    		result += "*";
+    	result += " A\n";
+    	for(int i = 1; i <= mNumberBs; i++)
+    		result += "*";
+    	result += " B\n";
+    	for(int i = 1; i <= mNumberCs; i++)
+    		result += "*";
+    	result += " C\n";
+    	for(int i = 1; i <= mNumberDs; i++)
+    		result += "*";
+    	result += " D\n";
+    	for(int i = 1; i <= mNumberFs; i++)
+    		result += "*";
+    	result += " F\n";
+    	
+    	
+    	return result;
+    	
+    }
     
+    public int getNumberOfGrades(){
+    	int result = 0;
+    	result += mNumberAs;
+    	result += mNumberBs;
+    	result += mNumberCs;
+    	result += mNumberDs;
+    	result += mNumberFs;
+    	
+    	return result;
+    }
     
 }
---
+++ IC14_GradeDistrubution/src/edu/orangecoastcollege/cs170/jgordon41/ic15/GradeDistributionDemo.java
@@ -0,0 +1,27 @@
+/**
+ * Gordon, Joel
+ * CS170
+ * Mar-22-2017
+ * 
+ * IC14_GradeDistrubution
+ */
+package edu.orangecoastcollege.cs170.jgordon41.ic15;
+
+/**
+ * @author Joel
+ *
+ */
+public class GradeDistributionDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GradeDistribution grade = new GradeDistribution(5, 6, 4, 7, 9);
+		
+		
+		System.out.println(grade);
+
+	}
+
+}
