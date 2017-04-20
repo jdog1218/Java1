46448916c217de276e042c9ae84163cab1d13547
jgordon41 <jgordon41@OVDILAB-9V.COAST.CCCD.ADX>
2017-04-20 14:00:38 -0700

    used for practice it

--- TestNumberFormats/src/TestNumberFormats.java
+++ TestNumberFormats/src/TestNumberFormats.java
@@ -11,20 +11,22 @@ public class TestNumberFormats {
 
 	public static void main(String[] args) {
 
-		// define a double varibale
-		double number = 12345.6789;
-		System.out.println("Original: " + number);
-		// two numberFormats built into java
-		// 1)currency - puts currency symbol and amount with 2 decimlas places
-		// (rounds)
-		NumberFormat currency = NumberFormat.getCurrencyInstance();
-		System.out.println("Currency Format: " + currency.format(number));
-		// 2) percent - multiples number by 100 and puts % symbol after it
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		System.out.println("Percent: " + percent.format(number));
-		// 3)1 decimal place
-		DecimalFormat oneDP = new DecimalFormat("#.0");
-		System.out.println("1 Decimal Place: " + oneDP.format(number));
+		int number = 8;
+		halfTheFun(11);
+		halfTheFun(2-3+2*8);
+		halfTheFun(number);
+		System.out.println("number = " + number);
+		
+	}
+	
+	public static void halfTheFun(int number){
+	    number /= 2;
+	    for (int count = 1; count <= number; count++){
+	        System.out.print(count + " ");
+	        
+	    }
+	    System.out.println();
+	    
 	}
 
 }
